71794b79af607148d2e99b0c4e1e6099cb666070
Marcus Costa <oc.marcus@gmail.com>
2019-05-13 16:03:27 -0300

    Ajustando a leitura das urls

--- ApolloServer/src/commons/config/index.js
+++ ApolloServer/src/commons/config/index.js
@@ -1,27 +1,13 @@
-const conf = require('nconf');
-const logger = require('../logger');
+const nconf = require('nconf');
 
-const LINKS = [];
+nconf.argv()
+  .env()
+  .defaults({
+    PORT: 5005,
+    PRECO_INTROSPECT_URL: 'http://localhost:8005',
+    PRODUTO_INTROSPECT_URL: 'http://localhost:8006',
+    PATH_GRAPHQL: '/graphql',
+    ROOT_PATH: '/apollo',
+  });
 
-conf.argv()
-  .env({
-    transform(obj) {
-      if ((new RegExp('_INTROSPECT_URL')).test(obj.key)) {
-        logger.info(`Variável: ${obj.key} url: ${obj.value}`);
-        LINKS.push(obj.value);
-      }
-      return obj;
-    },
-  })
-  .defaults(
-    {
-      PORT: 5005,
-      PRECO_INTROSPECT_URL: 'http://localhost:8005',
-      PRODUTO_INTROSPECT_URL: 'http://localhost:8006',
-      LINKS,
-      PATH_GRAPHQL: '/graphql',
-      ROOT_PATH: '/apollo',
-    },
-  );
-
-module.exports = conf;
+module.exports = nconf;
--- ApolloServer/src/schemas/introspect.schemas.js
+++ ApolloServer/src/schemas/introspect.schemas.js
@@ -3,7 +3,11 @@ const { HttpLink } = require('apollo-link-http');
 const fetch = require('node-fetch');
 const commons = require('../commons');
 
-const links = commons.conf.get('LINKS')
+const getUrls = () => Object.keys(commons.conf.get())
+  .filter(name => new RegExp('_INTROSPECT_URL').test(name))
+  .map(name => commons.conf.get(name));
+
+const urls = getUrls()
   .map(link => new HttpLink({ uri: `${link}${commons.conf.get('PATH_GRAPHQL')}`, fetch }));
 
 const createExecutableSchema = async (link) => {
@@ -15,4 +19,4 @@ const createExecutableSchema = async (link) => {
   });
 };
 
-module.exports = async () => Promise.all(links.map(link => createExecutableSchema(link)));
+module.exports = async () => Promise.all(urls.map(link => createExecutableSchema(link)));
